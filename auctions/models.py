c82f71407d0c938ab3bd286de9bc78eb29cfc1cb
Suguru Miyata <suguru1114@gmail.com>
2023-07-31 12:57:49 +1000

    add specification in models.py

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,7 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+# i will need to add additional models to this file to represent details about auction listings,
+# bids, comments, and auction categories.  
