03fb9e24922ac19ea3dc511c46174620b66761f4
Suguru Miyata <suguru1114@gmail.com>
2023-08-08 12:47:49 +1000

    add itemlist in model

--- auctions/models.py
+++ auctions/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class aucton_listing(models.Models):
+class Aucton_listing(models.Model):
+    item_list = models.CharField(max_length=64)
 
 # auction listing 
 # bids
@@ -14,3 +15,4 @@ class aucton_listing(models.Models):
 
 # i will need to add additional models to this file to represent details about auction listings,
 # bids, comments, and auction categories.  
+# 1;34 many to many 
